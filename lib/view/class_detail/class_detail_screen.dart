6b5d64259633d8e4db8a1e21a13d02d5d9829ada
rickyprimay <rickyprima30@gmail.com>
2025-05-07 23:36:07 +0700

    showing message chatbot

--- lib/view/class_detail/class_detail_screen.dart
+++ lib/view/class_detail/class_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:widya/view/class_detail/widget/chat_pop_up.dart';
 import 'package:widya/viewModel/in_class_view_model.dart';
+import 'package:widya/viewModel/lesson_view_model.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:widya/res/widgets/colors.dart';
 import 'package:widya/res/widgets/fonts.dart';
@@ -12,7 +14,7 @@ class ClassDetailScreen extends StatefulWidget {
 }
 
 class _ClassDetailScreenState extends State<ClassDetailScreen> {
-  InClassViewModel _viewModel = InClassViewModel();
+  final LessonViewModel viewModel = LessonViewModel();
   final List<Map<String, dynamic>> lectures = [
     {
       'title': 'Teknik Fingerpicking Lanjutan',
@@ -92,113 +94,9 @@ class _ClassDetailScreenState extends State<ClassDetailScreen> {
     super.dispose();
   }
 
-  void _openChat() {
-  final TextEditingController _chatController = TextEditingController();
-
-  showModalBottomSheet(
-    context: context,
-    isScrollControlled: true,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-    ),
-    builder: (context) {
-      return DraggableScrollableSheet(
-        expand: false,
-        initialChildSize: 0.6,
-        minChildSize: 0.4,
-        maxChildSize: 0.9,
-        builder: (context, scrollController) {
-          return Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: Text(
-                  'Chat dengan WiChat',
-                  style: AppFont.crimsonTextHeader.copyWith(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              const Divider(height: 1),
-              Expanded(
-                child: ListView.builder(
-                  controller: scrollController,
-                  itemCount: 1, // You can expand this later
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: const CircleAvatar(
-                        backgroundImage: AssetImage('assets/common/chatbot.png'),
-                      ),
-                      title: Text(
-                        'WiChat', 
-                        style: AppFont.ralewaySubtitle.copyWith(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        )
-                      ),
-                      subtitle: Text(
-                        'Halo, aku WiChat, asisten virtualmu. Apa yang bisa aku bantu hari ini?',
-                        style: AppFont.nunitoSubtitle.copyWith(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const Divider(height: 1),
-              Padding(
-                padding: MediaQuery.of(context).viewInsets.add(
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _chatController,
-                        decoration: InputDecoration(
-                          hintText: 'Ketik pesan...',
-                          hintStyle: AppFont.ralewaySubtitle.copyWith(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.grey,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 8,
-                          ),
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.send, color: AppColors.primary),
-                      onPressed: () {
-                        // Trigger content generation when message is sent
-                        _viewModel.generateContent(_chatController.text).then((_) {
-                          setState(() {
-                            // Update the chat with generated content here
-                          });
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          );
-        },
-      );
-    },
-  );
-}
+  _openChat() {
+    showChatPopUp(context);
+  }
 
 
   @override
