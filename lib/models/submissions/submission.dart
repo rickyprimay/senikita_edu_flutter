7072f742b411c7527a3a5255ec3e5b73c1028a14
rickyprimay <rickyprima30@gmail.com>
2025-05-12 03:48:44 +0700

    make model submission

--- lib/models/submissions/submission.dart
+++ lib/models/submissions/submission.dart
@@ -5,6 +5,15 @@ class Submission {
         required this.data,
     });
 
+    factory Submission.fromJson(Map<String, dynamic> json) {
+        return Submission(
+            data: List<Datum>.from(json['data'].map((x) => Datum.fromJson(x))),
+        );
+    }
+
+    Map<String, dynamic> toJson() => {
+        'data': List<dynamic>.from(data.map((x) => x.toJson())),
+    };
 }
 
 class Datum {
@@ -34,4 +43,33 @@ class Datum {
         required this.isPublished,
     });
 
+    factory Datum.fromJson(Map<String, dynamic> json) {
+        return Datum(
+            id: json['id'],
+            userId: json['user_id'],
+            submission: json['submission'],
+            filePath: json['file_path'],
+            status: json['status'],
+            feedback: json['feedback'],
+            score: json['score'],
+            createdAt: DateTime.parse(json['created_at']),
+            updatedAt: DateTime.parse(json['updated_at']),
+            lessonId: json['lesson_id'],
+            isPublished: json['is_published'],
+        );
+    }
+
+    Map<String, dynamic> toJson() => {
+        'id': id,
+        'user_id': userId,
+        'submission': submission,
+        'file_path': filePath,
+        'status': status,
+        'feedback': feedback,
+        'score': score,
+        'created_at': createdAt.toIso8601String(),
+        'updated_at': updatedAt.toIso8601String(),
+        'lesson_id': lessonId,
+        'is_published': isPublished,
+    };
 }
